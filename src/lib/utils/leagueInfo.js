4a6421aebdae7686aa1cc7a3f741d01ab0b964c9
WayOutHereInTheVoid <emeraldstarfollower@gmail.com>
2024-08-22 00:10:38 -0700

    Enabled blog

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -3,7 +3,7 @@ export const leagueID = "1124822402371428352"; // your league ID
 export const leagueName = "The Real League"; // your league name
 export const dues = 200; // (optional) used in template constitution page
 export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
-export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
+export const enableBlog = true; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
 /*   STEP 2   */
 export const homepageText = `
