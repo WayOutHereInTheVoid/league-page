e60f387f646811536824486b210e1aa3ebed0a79
WayOutHereInTheVoid <emeraldstarfollower@gmail.com>
2025-06-21 16:11:12 -0700

    color updates

--- src/lib/Nav/index.svelte
+++ src/lib/Nav/index.svelte
@@ -35,8 +35,8 @@
 		background-color: var(--fff);
 		position: relative;
 		z-index: 2;
-		border-bottom: 1px solid #00316b;
-		box-shadow: 0 0 8px 0 #00316b;
+		border-bottom: 1px solid #4CAF50;
+		box-shadow: 0 0 8px 0 #4CAF50;
 	}
 
 	#logo {
