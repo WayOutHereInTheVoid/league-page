8ffb07a884b302bd2a246edc0ae2a42eea03ba17
WayOutHereInTheVoid <emeraldstarfollower@gmail.com>
2025-08-31 14:44:22 -0700

    remove upcoming drafts on drafts page

--- src/lib/Drafts/index.svelte
+++ src/lib/Drafts/index.svelte
@@ -3,7 +3,7 @@
     import LinearProgress from '@smui/linear-progress';
     import Draft from './Draft.svelte'; 
 
-    export let upcomingDraftData, previousDraftsData, leagueTeamManagersData, playersData;
+    export let previousDraftsData, leagueTeamManagersData, playersData;
 </script>
 
 <style>
@@ -23,24 +23,7 @@
     }
 </style>
 
-
-{#await waitForAll(upcomingDraftData, leagueTeamManagersData, playersData) }
-	<div class="loading">
-		<p>Retrieving upcoming draft...</p>
-		<br />
-		<LinearProgress indeterminate />
-	</div>
-{:then [upcomingDraft, leagueTeamManagers, {players}] }
-    <h4>Upcoming {upcomingDraft.year} Draft</h4>
-    <Draft draftData={upcomingDraft} {leagueTeamManagers} year={upcomingDraft.year} {players} />
-{:catch error}
-	<!-- promise was rejected -->
-	<p>Something went wrong: {error.message}</p>
-{/await}
-
-
 {#await waitForAll(previousDraftsData, leagueTeamManagersData, playersData) }
-	<hr />
 	<h4>Previous Drafts</h4>
 	<div class="loading">
 		<p>Retrieving previous drafts...</p>
@@ -50,7 +33,6 @@
 {:then [previousDrafts, leagueTeamManagers, {players}] }
 	<!-- Don't display anything unless there are previous drafts -->
 	{#if previousDrafts.length}
-		<hr />
 		<h4>Previous Drafts</h4>
 		{#each previousDrafts as previousDraft}
 			<h6>{previousDraft.year} Draft</h6>
